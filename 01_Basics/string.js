8630bdd68d79f060b7b5e5c55b3852cb176536ff
GarvitDwivedi <112751660+GarvitDwivedi@users.noreply.github.com>
2024-07-29 10:46:39 +0000

    learn String

---
+++ 01_Basics/string.js
@@ -0,0 +1,36 @@
+const name = "Garvit Dwivedi" ;
+const value = 50 ;
+// console.log(name + value + " ans");
+
+const gameName = new String("_AEFG_");
+// console.log(gname
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('G'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(1, 6)
+// console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '****'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
