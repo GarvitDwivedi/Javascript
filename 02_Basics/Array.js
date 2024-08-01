a241b6cc0e9431e412bb5f002b6036d426a11fb8
GarvitDwivedi <112751660+GarvitDwivedi@users.noreply.github.com>
2024-08-01 06:01:14 +0000

    LearnBasicofArray

---
+++ 02_Basics/Array.js
@@ -0,0 +1,43 @@
+// const arr = [1,2,3,4,5,6]
+// console.log(arr);
+
+// const newarr = ["Harray" , "CodeandCHAI" , {1:4 , 3 : 4} , 2]
+// console.log(newarr);
+
+// console.log(arr[3]);
+
+// // ArraY Methods 
+
+// arr.push(40) ;
+// console.log(arr);
+
+// arr.pop() ;
+// arr.pop() ;
+// console.log(arr);
+
+
+// arr.unshift(3)
+// console.log(arr);
+// arr.unshift(4);
+// console.log(arr);//
+
+// console.log(arr.shift());
+
+// console.log(arr.includes(7));
+// console.log(arr.indexOf(3));
+
+// const ansarr = arr.join();
+// console.log(typeof ansarr);
+
+
+
+// Slice 
+const arr = [1,2,3,4,5,6,7,8]
+const ans = arr.slice(-6 , 9)
+console.log(ans);
+
+
+//splice
+
+arr.splice(5,6)
+console.log(arr);
