e0bea161ddbee7ed3381147169796f2504ddc5e6
GarvitDwivedi <112751660+GarvitDwivedi@users.noreply.github.com>
2024-08-01 05:09:32 +0000

    date object done

---
+++ 01_Basics/dateobject.js
@@ -0,0 +1,17 @@
+let date = new Date() ;
+console.log(date);
+
+// console.log(date.toDateString()) ;
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toUTCString());
+// console.log(date.toTimeString ());
+
+// let myCreatedDate = new Date(2023, 0, 23)
+ //let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+ //let myCreatedDate = new Date("2023-01-14")
+//  let myCreatedDate = new Date("01-14-2023")
+//  console.log(myCreatedDate.toLocaleString());
