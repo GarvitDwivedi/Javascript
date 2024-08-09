726c694cbbbe53ce6fc33dac2c73371abfabcdbb
GarvitDwivedi <112751660+GarvitDwivedi@users.noreply.github.com>
2024-08-09 08:18:09 +0000

    Learned iffi

---
+++ 03_Basics/life.js
@@ -0,0 +1,13 @@
+// Immediately invoked Function  // IIFI 
+
+(function chai(){
+    console.log("Helper");
+    
+})();
+
+
+
+(function helper(name){
+    console.log(`My name is ${name}`);
+    
+})("Harvit")
