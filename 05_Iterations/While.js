84343836822e1f35d5f42c03d2400d569d21246f
GarvitDwivedi <112751660+GarvitDwivedi@users.noreply.github.com>
2024-08-11 09:45:56 +0000

    While loop

---
+++ 05_Iterations/While.js
@@ -0,0 +1,21 @@
+// let i = 0 ;
+// while(i < 5){
+//     console.log(i); 
+//     i++;
+// }
+
+
+const arr = ["Mahadv" , "MataParvati" , "Narayan"] 
+
+let j = 0 ;
+// while(j < arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+do{
+    console.log(arr[j]);
+    j++;
+    
+}while(j < arr.length-3)
+
